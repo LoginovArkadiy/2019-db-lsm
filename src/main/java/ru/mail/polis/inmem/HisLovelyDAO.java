bc82688938dae98c06a8464de64e01c0812432e9
Loginov_Arkadiy <loginov3.aa@edu.spbstu.ru>
2019-04-16 21:29:08 +0300

    add finals

--- src/main/java/ru/mail/polis/inmem/HisLovelyDAO.java
+++ src/main/java/ru/mail/polis/inmem/HisLovelyDAO.java
@@ -15,17 +15,17 @@ public class HisLovelyDAO implements DAO {
 
     @NotNull
     @Override
-    public Iterator<Record> iterator(@NotNull ByteBuffer from) {
+    public Iterator<Record> iterator(@NotNull final ByteBuffer from) {
         return map.tailMap(from).values().iterator();
     }
 
     @Override
-    public void upsert(@NotNull ByteBuffer key, @NotNull ByteBuffer value) {
+    public void upsert(@NotNull final ByteBuffer key, @NotNull final ByteBuffer value) {
         map.put(key, Record.of(key, value));
     }
 
     @Override
-    public void remove(@NotNull ByteBuffer key) {
+    public void remove(@NotNull final ByteBuffer key) {
         map.remove(key);
     }
 
