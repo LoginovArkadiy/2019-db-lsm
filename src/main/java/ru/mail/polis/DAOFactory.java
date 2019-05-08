8d338252810ee4523577822cc42ce2b253c5f572
Loginov_Arkadiy <loginov3.aa@edu.spbstu.ru>
2019-05-08 23:59:43 +0300

    fix

--- src/main/java/ru/mail/polis/DAOFactory.java
+++ src/main/java/ru/mail/polis/DAOFactory.java
@@ -55,6 +55,6 @@ final class DAOFactory {
             throw new IllegalArgumentException("Path is not a directory: " + data);
         }
 
-        return new LSMDao(data, MAX_HEAP / 64);
+        return new LSMDao(data, MAX_HEAP / 60);
     }
 }
